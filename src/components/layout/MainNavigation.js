4b4faf5763878e88ba84ad675fee4ff9d917cc6b
Onesmo Kasongo <kasongoonesmo33@gmail.com>
2021-10-13 23:53:52 +0530

    added pages, styling

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>MeetUps</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetups">Add New Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favourites">Favourite Meetups</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
---
+++ src/pages/Favourites.js
@@ -0,0 +1,5 @@
+function FavouritesPage() {
+  return <div>Favourites</div>;
+}
+
+export default FavouritesPage;
